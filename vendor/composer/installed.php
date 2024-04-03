a31edda196fea878e67f9cc3663ba410e8733e27
Krishna Btist <krishnabtist@gmail.com>
2024-04-03 09:48:39 +0530

    git pull code - API
    git pull code - API

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'czproject/git-php' => array(
+            'pretty_version' => 'v4.2.0',
+            'version' => '4.2.0.0',
+            'reference' => 'e257f2c3b43fe8fef19ddb5727b604416b423107',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../czproject/git-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
