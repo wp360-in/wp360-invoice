4facaa2cdbdfe79fb10a6e5b7d757092d9801d31
Krishna Btist <krishnabtist@gmail.com>
2024-04-29 12:04:41 +0530

    clean update plugin file
    clean update plugin file

--- custom_plugin_update_count.php
+++ custom_plugin_update_count.php
@@ -1,47 +1,9 @@
 <?php
 
-// add_filter('pre_set_site_transient_update_plugins', 'custom_plugin_update_count');
-// function custom_plugin_update_count($transient) {
-//     // $custom_plugin_file = 'wp360-invoice/wp360-invoice.php';
-//     // $available_version = get_option('wp360_plugin_available_version');
-//     // $installed_version = get_plugin_data(WP_PLUGIN_DIR . '/' . $custom_plugin_file)['Version'];
-
-//  $plugin_basename = plugin_basename(__FILE__); // Get the plugin's basename
-//  $custom_plugin_file = dirname($plugin_basename) . '/wp360-invoice.php'; // Combine with '/wp360-invoice.php'
-//  $available_version = get_option('wp360_plugin_available_version');
-//     $installed_version = get_plugin_data(WP_PLUGIN_DIR . '/' . $custom_plugin_file)['Version'];
-//     if (!empty($available_version) && version_compare($available_version, $installed_version, '>')) {
-//         // Increment the update count
-
-//      $res = new stdClass();
-//      $res->slug = dirname($plugin_basename); // Extract the directory name as the slug
-//      $res->plugin = $custom_plugin_file;
-//      $res->new_version = $available_version;
-//      $res->tested = 'tester';
-//      $plugin_slug    = basename(dirname(__FILE__));
-//         $res->package = 'https://github.com/KrishnaBtist/wp360-invoice-btist/archive/refs/tags/1.1.2.zip';
-//      $transient->response[$res->plugin] = $res;
-
-
-
-
-//         // if (!isset($transient->response[$custom_plugin_file])) {
-//         //     $transient->response[$custom_plugin_file] = new stdClass();
-//         // }
-//         // $transient->response[$custom_plugin_file]->new_version = $available_version;
-
-
-//     }
-//     return $transient;
-// }
-
-
 // add_action('admin_init', 'clear_plugin_updates');
 // function clear_plugin_updates() {
 //     delete_site_transient('update_plugins');
 // }
-
-
 add_filter( 'site_transient_update_plugins', 'wp360_push_update' );
 function wp360_push_update( $transient ){
     if ( empty( $transient->checked ) ) {
@@ -58,31 +20,14 @@ function wp360_push_update( $transient ){
         $res->new_version = $available_version;
         $res->tested = 'tester';
         $plugin_slug    = basename(dirname(__FILE__));
-      //  $res->package = 'https://github.com/';
         $transient->response[$res->plugin] = $res;
-        //activate_plugin( $plugin_slug); // Reactivate
     }
     return $transient;
 }
 
-// add_action('activated_plugin', 'wp360_after_plugin_activation', 10, 2);
-// function wp360_after_plugin_activation( $plugin, $network_activation ) {
-//     if ( $plugin == plugin_basename(__FILE__) ) {
-//         // Plugin is activated, perform any actions here
-//         // For example, redirect the user to a specific page
-//         wp_redirect('https://staging.wp360.in/wp-admin/plugins.php');
-//         exit;
-//     }
-// }
-
-
 
 add_filter( 'plugins_api', 'wp360_plugin_info', 20, 3);
-/*
- * $res empty at this step
- * $action 'plugin_information'
- * $args stdClass Object ( [slug] => woocommerce [is_ssl] => [fields] => Array ( [banners] => 1 [reviews] => 1 [downloaded] => [active_installs] => 1 ) [per_page] => 24 [locale] => en_US )
- */
+
 function wp360_plugin_info( $res, $action, $args ){
     // do nothing if this is not about getting plugin information
     // if( 'plugin_information' !== $action ) {
--- wp360_update.php
+++ wp360_update.php
@@ -109,15 +109,10 @@ function fetchFilesFromDirectory($client, $apiUrl, $localDirectory, $token) {
        // 'Authorization' => 'token ' . $token,
         'Accept' => 'application/vnd.github.v3+json',
     ];
-    // Send request to GitHub API to get repository contents
     $response = $client->request('GET', $apiUrl, [
         'headers' => $headers,
     ]);
-   // echo '<pre>', var_dump( $response ); echo '</pre>';
     $files = json_decode($response->getBody(), true);
-  //  echo '<pre>', var_dump( $files ); echo '</pre>';
-
-    // Iterate through each file in the repository
     foreach ($files as $file) {
         if ($file['type'] === 'file') {
             $fileContent = file_get_contents($file['download_url']);
@@ -140,58 +135,4 @@ function fetchFilesFromDirectory($client, $apiUrl, $localDirectory, $token) {
         }
     }
 }
-
-
-
-// Add action hook to wp_head
-// add_action('wp_head', function() {
-//     $plugin_dir = plugin_dir_path(__FILE__);
-//     require_once $plugin_dir . 'vendor/autoload.php';
-//     $repoOwner = 'KrishnaBtist';
-//     $repoName = 'wp360-invoice-btist';
-//     $branch = 'main'; 
-//     $token = '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'; // Replace this with your actual personal access token
-//     // GitHub API endpoint to get the repository contents
-//     $apiUrl = "https://api.github.com/repos/{$repoOwner}/{$repoName}/contents";
-//     // Path where you want to clone the repository
-//     $clonePath = plugin_dir_path(__FILE__);
-//     // Initialize GuzzleHttp client
-//     $client = new GuzzleHttp\Client();
-//     // Function to fetch files from a directory
-//     function fetchFilesFromDirectory($client, $apiUrl, $localDirectory, $token) {
-//         $headers = [
-//             'Authorization' => 'token ' . $token,
-//             'Accept' => 'application/vnd.github.v3+json',
-//         ];
-//         // Send request to GitHub API to get repository contents
-//         $response = $client->request('GET', $apiUrl, [
-//             'headers' => $headers,
-//         ]);
-//         $files = json_decode($response->getBody(), true);
-//         // Iterate through each file in the repository
-//         foreach ($files as $file) {
-//             if ($file['type'] === 'file') {
-//                 $fileContent = file_get_contents($file['download_url']);
-//                 $localFilePath = $localDirectory . '/' . $file['name'];
-//                 if (file_exists($localFilePath)) {
-//                     file_put_contents($localFilePath, $fileContent);
-//                     error_log(' File '{$file['name']}' updated locally 1.');
-//                 } else {
-//                     file_put_contents($localFilePath, $fileContent);
-//                     error_log('File '{$file['name']}' saved locally 1 <br>')
-//                 }
-//             } elseif ($file['type'] === 'dir') {
-//                 $subDirectoryUrl = $file['url'];
-//                 $subDirectoryName = $file['name'];
-//                 $subLocalDirectory = $localDirectory . '/' . $subDirectoryName;
-//                 if (!file_exists($subLocalDirectory)) {
-//                     mkdir($subLocalDirectory, 0777, true);
-//                 }
-//                 fetchFilesFromDirectory($client, $subDirectoryUrl, $subLocalDirectory, $token);
-//             }
-//         }
-//     }
-//     fetchFilesFromDirectory($client, $apiUrl, $clonePath, $token);
-// });
-
 
